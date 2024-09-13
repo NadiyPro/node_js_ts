792e838d12644cc0b46b18ab4ddddf72122fe3c3
NadiyPro <siroviyn13@gmail.com>
2024-09-14 01:09:04 +0300

    create

--- src/router/router.ts
+++ src/router/router.ts
@@ -9,7 +9,7 @@ const router = Router();
 
 router.get("/", userController.getUsers);
 router.post("/", userMiddleware.validateUser, userController.postUser);
-//
+
 router.get("/:userId", userController.getUserId);
 router.put("/:userId", userMiddleware.validateUser, userController.updateUser);
 router.delete("/:userId", userController.deleteUser);
