5e920d7ea48a7bd2b8452b1b7e52d2b56e35aff6
NadiyPro <siroviyn13@gmail.com>
2024-09-13 23:54:25 +0300

    create

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 
 import { ApiError } from "./api.error";
 import { userRouter } from "./router/router";
@@ -11,12 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/users", userRouter);
 
 //обробка помилок які ми витягнули сюди (на верхній рівень) через next()
-app.use(
-  "*",
-  (error: ApiError, req: Request, res: Response, next: NextFunction) => {
-    res.status(error.status || 500).send(error.message);
-  },
-);
+app.use("*", (error: ApiError, req: Request, res: Response) => {
+  res.status(error.status || 500).send(error.message);
+});
 // на якому порті відкриваємо (номер хосту)
 const PORT = 3001;
 app.listen(PORT, () => {
