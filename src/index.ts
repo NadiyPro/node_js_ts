7591ffa7879846d37035d4b81df5aeea17955a5e
NadiyPro <siroviyn13@gmail.com>
2024-09-13 21:05:23 +0300

    create

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fs from "node:fs/promises";
 import express, { NextFunction, Request, Response } from "express";
 import path from "path";
 
+import { ApiError } from "./api.error";
 import { IUser } from "./interfaces/IUser";
 import { users } from "./users_array";
 
@@ -130,15 +131,15 @@ app.delete(
   },
 );
 //обробка помилок які ми витягнули сюди (на верхній рівень) через next(e)
-app.use("*", (error: Error, req: Request, res: Response) => {
-  res.status(500).send(error.message);
-});
-// app.use(
-//     "*",
-//     (error: ApiError, req: Request, res: Response, next: NextFunction) => {
-//       res.status(error.status || 500).send(error.message);
-//     },
-// );
+// app.use("*", (error: Error, req: Request, res: Response) => {
+//   res.status(500).send(error.message);
+// });
+app.use(
+  "*",
+  (error: ApiError, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500).send(error.message);
+  },
+);
 // на якому порті відкриваємо (номер хосту)
 const PORT = 3001;
 app.listen(PORT, () => {
