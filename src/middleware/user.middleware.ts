471550ac71772b8138dfec5509e096af0b0491a8
NadiyPro <siroviyn13@gmail.com>
2024-09-13 23:42:22 +0300

    create

--- src/middleware/user.middleware.ts
+++ src/middleware/user.middleware.ts
@@ -6,11 +6,11 @@ class UserMiddleware {
     const { name, age, status } = req.body;
 
     const isValidName = typeof name === "string" && name.length <= 20;
-    const isValidAge = typeof age === "number" && age <= 100;
+    const isValidAge = typeof age === "number" && age > 0 && age <= 100;
     const isValidStatus = typeof status === "boolean";
 
     if (isValidName && isValidAge && isValidStatus) {
-      next(); // Якщо дані валідні, переходимо далі
+      next(); // Якщо дані валідні, переходимо далі в контролер
     } else {
       res.status(400).send("Invalid user");
     }
