f51b7eddcf307aa66d7bc6443bb36ff30bb07e8b
NadiyPro <siroviyn13@gmail.com>
2024-09-17 20:02:42 +0300

    create

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -5,4 +5,6 @@ dotenv.config();
 export const config = {
   APP_PORT: process.env.APP_PORT || 3002,
   APP_HOST: process.env.APP_HOST,
+
+  MONGO_URI: process.env.MONGO_URI,
 };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express, { Request, Response } from "express";
 
 import { ApiError } from "./errors/api.error";
 import { userRouter } from "./router/router";
-import {config} from "./config/configs";
+import * as mongoose from "mongoose";
+import { config } from "./config/configs";
 
 const app = express();
 
@@ -16,6 +17,9 @@ app.use("*", (error: ApiError, req: Request, res: Response) => {
   res.status(error.status || 500).send(error.message);
 });
 // на якому порті відкриваємо (номер хосту)
-app.listen(config.APP_PORT, () => {
-  console.log(`Server is running on https://${config.APP_HOST}:${config.APP_PORT}`);
+app.listen(config.APP_PORT, async () => {
+  await mongoose.connect(config.MONGO_URI);
+  console.log(
+      `Server is running on https://${config.APP_HOST}:${config.APP_PORT}`,
+  );
 });
