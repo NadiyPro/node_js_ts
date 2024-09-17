e138c4eeaf6695311567fbaba773b051a124703b
NadiyPro <siroviyn13@gmail.com>
2024-09-17 17:12:36 +0300

    create

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -4,4 +4,5 @@ dotenv.config();
 
 export const config = {
   APP_PORT: process.env.APP_PORT || 3002,
+  APP_HOST: process.env.APP_HOST,
 };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 
 import { ApiError } from "./errors/api.error";
 import { userRouter } from "./router/router";
+import {config} from "./config/configs";
 
 const app = express();
 
@@ -15,7 +16,6 @@ app.use("*", (error: ApiError, req: Request, res: Response) => {
   res.status(error.status || 500).send(error.message);
 });
 // на якому порті відкриваємо (номер хосту)
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running ${PORT}`);
+app.listen(config.APP_PORT, () => {
+  console.log(`Server is running on https://${config.APP_HOST}:${config.APP_PORT}`);
 });
