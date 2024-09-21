d8c2cac17ce37a509ce402b9c2d9b3fb16fa789e
NadiyPro <siroviyn13@gmail.com>
2024-09-21 15:31:35 +0300

    create

---
+++ src/services/password.service.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt";
+
+class PasswordService {
+  async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 10); // хешуємо введений першочерговий password, ставимо на  10 раундів
+  }
+
+  async comparePassword(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword); // порівнюємо новий введений пароль з раніше введеним паролем
+  }
+}
+// bcrypt.compare (процес порівнння хешів), для цього:
+// - витягається сіль з збереженого хешу.
+// - введений користувачем пароль хешується з цією сіллю.
+// - порівнюються результуючі хеші.
+
+export const passwordService = new PasswordService();
